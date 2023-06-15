b0afc19f01682613508bb2f29d1f5d482bbb0607
Arild Matsson <arild.matsson@gu.se>
2023-06-15 16:55:23 +0200

    Super simple jwts

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
+class Jwt(BaseModel):
+    sub: str
     name: str
-    price: float
-    is_offer: Union[bool, None] = None
+    iat: int
+
+  
+store = dict()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Fakejwt: Test your web apps against customized jwts! See https://github.com/spraakbanken/fakejwt"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/jwt")
+def get_jwt(id: int):
+    return store[id]
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.put("/jwt")
+def put_jwt(jwt: Jwt):
+    id = len(store)
+    store[id] = jwt
+    return {"id": id}
