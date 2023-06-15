ea7060d9e46304fc7b7568e2f2196dcfbd286555
Arild Matsson <arild.matsson@gu.se>
2023-06-15 17:41:51 +0200

    Error test cases

--- app/test_main.py
+++ app/test_main.py
@@ -18,3 +18,11 @@ def test_put_get():
     response = client.get('/jwt', params={"id": 0})
     assert response.status_code == 200
     assert response.json() == jwt
+
+def test_put_bad():
+    response = client.put('/jwt', content="foobar")
+    assert response.status_code == 422
+
+def test_get_missing():
+    response = client.get('/jwt', params={"id": 123})
+    assert response.status_code == 404
